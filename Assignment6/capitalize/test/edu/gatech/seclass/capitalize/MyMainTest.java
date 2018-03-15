0e01c521fa7dbda662c97105df6ae7308898e520
sean <smatthews39@gatech.edu>
2018-03-14 18:19:43 -0700

    catpart files and capitalize project unmodified

---
+++ Assignment6/capitalize/test/edu/gatech/seclass/capitalize/MyMainTest.java
@@ -0,0 +1,17 @@
+package edu.gatech.seclass.capitalize;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class MyMainTest {
+	
+/*
+Place all  of your tests in this class, optionally using MainTest.java as an example.
+*/
+
+}
