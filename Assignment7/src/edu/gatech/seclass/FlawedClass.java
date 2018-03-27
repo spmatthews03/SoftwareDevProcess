4c504ae7626910a9a77d6993435e6e9c26c3b567
smatthews39 <smatthews39@gatech.edu>
2018-03-27 09:43:48 -0700

    secondary commit for assignment7

--- Assignment7/src/edu/gatech/seclass/FlawedClass.java
+++ Assignment7/src/edu/gatech/seclass/FlawedClass.java
@@ -12,9 +12,30 @@ public class FlawedClass {
     }
 
     public int flawedMethod2(int x, int y){
-        if( x == 0 || y != 0){
+
+        return 0;
+    }
+
+    public int flawedMethod3(int x, int y){
+
+        return 0;
+    }
+
+    public int flawedMethod4(int x, int y){
+        return 0;
+    }
+
+    public boolean flawedMethod5(boolean a, boolean b){
+        int x = 2;
+        int y = 4;
+        if(a)
+            x = 4;
+        else
             y = y/x;
-        }
-        return y;
+        if(b)
+            y -= x;
+        else
+            x += y;
+        return ((x/y)>0);
     }
 }
