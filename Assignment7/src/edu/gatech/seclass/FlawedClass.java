5ed7f26a3b46db1218a472059f7c832356c41279
smatthews39 <smatthews39@gatech.edu>
2018-03-28 14:33:35 -0700

    mods

--- Assignment7/src/edu/gatech/seclass/FlawedClass.java
+++ Assignment7/src/edu/gatech/seclass/FlawedClass.java
@@ -4,38 +4,64 @@ public class FlawedClass {
 
 
 
-    public int flawedMethod1(int x, int y){
-        if( x == 0 || y != 0){
-            y = y/x;
+    public int flawedMethod1(boolean a, boolean b){
+
+        int x = 1;
+
+        if(a == true)
+        {
+            x = x  + 1;
         }
-        return y;
+        if(b == true)
+        {
+            x = 10;
+        }
+        else
+        {
+            x = x - 2;
+        }
+
+        return 1 / x;
     }
 
-    public int flawedMethod2(int x, int y){
+    public int flawedMethod2(boolean a, boolean b){
+
+
+    }
+
+    public int flawedMethod3(boolean a, boolean b){
 
         return 0;
     }
 
-    public int flawedMethod3(int x, int y){
-
+    public int flawedMethod4(boolean a, boolean b){
         return 0;
     }
 
-    public int flawedMethod4(int x, int y){
-        return 0;
-    }
 
-    public boolean flawedMethod5(boolean a, boolean b){
+
+
+    public boolean flawedMethod5 (boolean a, boolean b) {
         int x = 2;
         int y = 4;
         if(a)
             x = 4;
         else
-            y = y/x;
+            y = y / x;
         if(b)
             y -= x;
         else
             x += y;
         return ((x/y)>0);
     }
+
+// | a | b |output|
+// ================
+// | T | T | E   |
+// | T | F | T   |
+// | F | T | E   |
+// | F | F | T   |
+// ================
+// Coverage required: _____path coverage_____
+
 }
